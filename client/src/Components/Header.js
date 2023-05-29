98c44b7c4f05747b5726f0dbc34445e1200ae5e3
Pobarul-islam <pobarul3@gmail.com>
2023-05-29 16:23:05 +0600

    modified

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,72 +1,84 @@
 import React from 'react';
-import { FaShoppingCart } from "react-icons/fa";
-import { Link } from 'react-router-dom';
-import { useAuth } from '../Context/auth';
-import { toast } from 'react-toastify';
-  import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-router-dom";
+import { useAuth } from "../Context/auth";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Header = () => {
-  const handleLogout = () => {
-    setAuth({
-      ...auth, user:null, token:''
-    })
-    localStorage.removeItem('auth');
-    toast.success("Logout successfully")
-  }
-  const [auth, setAuth] = useAuth();
-     const menuItems = (
-       <>
-         <li>
-           <Link to="/">Home</Link>
-         </li>
-         <li>
-           <Link to="/about">About</Link>
-         </li>
+   const handleLogout = () => {
+     setAuth({
+       ...auth,
+       user: null,
+       token: "",
+     });
+     localStorage.removeItem("auth");
+     toast.success("Logout successfully");
+   };
+   const [auth, setAuth] = useAuth();
+   const menuItems = (
+     <>
+       <li>
+         <Link to="/">Home</Link>
+       </li>
+       <li>
+         <Link to="/about">About</Link>
+       </li>
 
-         <li>
-           <Link to="/category">category</Link>
-         </li>
-         {!auth.user ? (
-           <>
-             <li>
-               <Link to="/register">Register</Link>
-             </li>
-             <li>
-               <Link to="/login">Login</Link>
-             </li>
-           </>
-         ) : (
-           <>
-             <div className="dropdown text-black">
-               <label tabIndex={0} className="btn  m-1">
-                 {auth?.user?.name}
-               </label>
-               <ul
-                 tabIndex={0}
-                 className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-               >
-                 <li>
-                   <a>Dashboard</a>
-                 </li>
-                 <li>
-                   <Link onClick={handleLogout} to="/login">
-                     Logout
-                   </Link>
-                 </li>
-               </ul>
-             </div>
-           </>
-         )}
-         <li>
-           <Link to="/cart">Cart (0)</Link>
-         </li>
-       </>
-     );
-    return (
-   
-        <div className="navbar uppercase  bg-slate-700 sticky top-0 z-10 text-white">
-          <div className="navbar-start">
-            <div className="dropdown text-white">
-              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+       <li>
+         <Link to="/category">category</Link>
+       </li>
+       {!auth.user ? (
+         <>
+           
+           <li>
+             <Link to="/login">Login</Link>
+           </li>
+         </>
+       ) : (
+         <>
+           
+         </>
+       )}
+      
+     </>
+   );
+  return (
+
+      <div className="navbar  bg-slate-700 ">
+        <div className="flex-1">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {menuItems}
+            </ul>
+          </div>
+          <Link className="btn btn-ghost normal-case text-xl text-white">Dolly shop</Link>
+        </div>
+
+        <div className="flex-none text-white">
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{menuItems}</ul>
+          </div>
+          <div className="dropdown dropdown-end">
+            <Link to="/cart" tabIndex={0} className="btn btn-ghost btn-circle">
+              <div className="indicator">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -78,30 +90,50 @@ const Header = () => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                   />
                 </svg>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-700  text-white rounded-box w-52"
-              >
-                {menuItems}
-              </ul>
-            </div>
-            <Link to={"/"} className="btn btn-ghost normal-case gap-3 text-xl">
-              <span className="text-red-400">
-                <FaShoppingCart />{" "}
-              </span>{" "}
-              Dolly Shop
+                <span className="badge badge-sm indicator-item">8</span>
+              </div>
             </Link>
+
+            <div
+              tabIndex={0}
+              className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+            ></div>
           </div>
-          <div className="navbar-end hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">{menuItems}</ul>
+          <div className="dropdown dropdown-end text-black">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                {auth?.user?.name}
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link  to="/profile">
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard">
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link onClick={handleLogout} to="/login">
+                  Logout
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
-   
-    );
+      </div>
+
+  );
 };
 
 export default Header;
