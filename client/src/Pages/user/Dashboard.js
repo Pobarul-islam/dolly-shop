d36fead83b5d62330174a056998580fff4ddbdcf
Pobarul-islam <pobarul3@gmail.com>
2023-06-01 14:33:55 +0600

    cru added

--- client/src/Pages/user/Dashboard.js
+++ client/src/Pages/user/Dashboard.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import Layout from '../../Components/Layout';
-import UserMenu from '../../Components/Layout/UserMenu';
-import { useAuth } from '../../Context/auth';
-
+import React from "react";
+import Layout from "../../Components/Layout";
+import UserMenu from "../../Components/Layout/UserMenu";
+import { useAuth } from "../../Context/auth";
 
 const Dashboard = () => {
   const [auth] = useAuth();
-    return (
-      <Layout>
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-6 m-3 p-3">
-          <div>
-            <UserMenu />
-          </div>
-          <div>
-            <h1 className=" border p-3 m-2">{auth?.user?.name} </h1>
-            <h1 className=" border p-3 m-2">{auth?.user?.email} </h1>
-            <h1 className=" border p-3 m-2">{auth?.user?.address} </h1>
-     
-          </div>
+  return (
+    <Layout>
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-6 m-3 p-3">
+        <div>
+          <UserMenu />
         </div>
-      </Layout>
-    );
+        <div>
+          <h1 className=" border p-3 m-2">{auth?.user?.name} </h1>
+          <h1 className=" border p-3 m-2">{auth?.user?.email} </h1>
+          <h1 className=" border p-3 m-2">{auth?.user?.address} </h1>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Dashboard;
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import morgan from "morgan";
-import authRoutes from './routes/authRoute.js'
+import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoutes.js'
 import cors from 'cors';
 // configure env
 dotenv.config();
@@ -21,6 +22,7 @@ app.use(morgan("dev"));
 
 // routes 
 app.use('/api/v1/auth', authRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
 // rest api
 app.get("/", (req, res) => {
