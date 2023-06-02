1af89055301071c3c5187d710091f1c0c5fa6eef
Pobarul-islam <pobarul3@gmail.com>
2023-06-02 12:58:22 +0600

    created database product

---
+++ controllers/productController.js
@@ -0,0 +1,48 @@
+import slugify from "slugify";
+import productModel from "../models/productModel.js";
+import fs from "fs";
+export const createProductController = async (req, res) => {
+  try {
+    const { name, description, price, category, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
+
+    // validation
+    switch (true) {
+      case !name:
+        return res.status(500).send({ error: "Name is Required" });
+
+      case !description:
+        return res.status(500).send({ error: "description is Required" });
+      case !price:
+        return res.status(500).send({ error: "price is Required" });
+      case !category:
+        return res.status(500).send({ error: "category is Required" });
+      case !quantity:
+        return res.status(500).send({ error: "quantity is Required" });
+      case photo && photo.size > 1000000:
+        return res
+          .status(500)
+          .send({ error: "Photo is reuired and should be less then 1mb" });
+    }
+
+    const products = new productModel({ ...req.fields, slug: slugify(name) });
+    if (photo) {
+      products.photo.data = fs.readFileSync(photo.path);
+      products.photo.contentType = photo.type;
+    }
+    await products.save();
+    res.status(201).send({
+      success: true,
+      message: "Product created successfully",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in creating porduct",
+      error,
+    });
+  }
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import morgan from "morgan";
 import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from "./routes/productRoutes.js"
 import cors from 'cors';
 // configure env
 dotenv.config();
@@ -23,6 +24,7 @@ app.use(morgan("dev"));
 // routes 
 app.use('/api/v1/auth', authRoutes);
 app.use("/api/v1/category", categoryRoutes);
+app.use('/api/v1/product', productRoutes)
 
 // rest api
 app.get("/", (req, res) => {
